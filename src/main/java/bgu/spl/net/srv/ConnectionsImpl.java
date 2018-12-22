bc95e493c0ae4bb6a0c0d0851aa9ec51b2c50d88
Tomer <dror.tomer.il@gmail.com>
2018-12-23 00:33:56 +0200

    first-3

--- src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -2,27 +2,35 @@ package bgu.spl.net.srv;
 
 import bgu.spl.net.api.bidi.Connections;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectionsImpl<T> implements Connections<T>
 {
-    private int connectionId;
-    private T msg;
+    private ConcurrentHashMap<Integer, ConnectionHandler<T>> id_Msg = new ConcurrentHashMap<Integer, ConnectionHandler<T>>();
+
+    public void addConnection ( int connectionId, ConnectionHandler<T> connectionHandler )
+    {
+        id_Msg.put( connectionId, connectionHandler );
+    }
 
     @Override
     public boolean send ( int connectionId, T msg )
     {
-        
         return false;
     }
 
     @Override
     public void broadcast ( T msg )
     {
-        this.msg = msg;
+        for ( ConcurrentHashMap.Entry<Integer, ConnectionHandler<T>> entry : id_Msg.entrySet() )
+        {
+            entry.getValue().send( msg );
+        }
     }
 
     @Override
     public void disconnect ( int connectionId )
     {
-        this.connectionId = connectionId;
+        id_Msg.remove( connectionId );
     }
 }
